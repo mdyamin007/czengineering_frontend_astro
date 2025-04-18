5df33280bc07d8fbd7ebfe9f8be954f8e5244e0d
mdyamin007 <mohammadyamin80@gmail.com>
2025-04-18 18:26:53 +0600

    feat: navbar made responsive for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { LayoutGridIcon, MenuIcon } from "lucide-react";
-import { useState } from "react";
+import { LayoutGridIcon, Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 export default function Navbar() {
-  const [open, setOpen] = useState(false);
-
   return (
-    <header className="border-b">
+    <header className="border-b shadow-md">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         {/* Logo */}
         <div className="flex items-center space-x-2">
-          <img src="/images/logo.png" alt="Industrie Logo" className="h-12" />
+          <a href="/">
+            <img src="/images/logo.png" alt="Industrie Logo" className="h-12" />
+          </a>
         </div>
 
         {/* Navigation */}
@@ -46,13 +46,33 @@ export default function Navbar() {
               </span>
             </Button>
           </a>
-          <LayoutGridIcon className="w-5 h-5 text-muted-foreground" />
         </div>
 
-        {/* Mobile menu button */}
-        <button className="md:hidden" onClick={() => setOpen(!open)}>
-          <MenuIcon className="w-6 h-6" />
-        </button>
+        {/* Mobile Drawer */}
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <LayoutGridIcon className="w-5 h-5 text-muted-foreground" />
+            </SheetTrigger>
+            <SheetContent side="left">
+              <div className="flex flex-col space-y-4 mt-6 mx-4 text-muted-foreground">
+                <a href="/" className="text-primary font-medium ">
+                  Home
+                </a>
+                <a href="/about">About</a>
+                <a href="/products">Products</a>
+                <a href="/projects">Projects</a>
+                <a href="/client-list">Client List</a>
+                <a href="/contact-us">Contact us</a>
+                <a href="/contact-us">
+                  <Button className="bg-blue-700 text-white hover:bg-blue-800 mt-4 hover:cursor-pointer">
+                    Get In Touch
+                  </Button>
+                </a>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </header>
   );
